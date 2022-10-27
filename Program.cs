0287739c46743f88e17b62423218d681c02c3ef0
ognjensvr <116808668+ognjensvr@users.noreply.github.com>
2022-10-27 13:47:30 +0200

    prvi projekat

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            for (int broj = 1; broj < 100; broj++)
+            {
+                if (broj % 5 == 0)
+                {
+                    Console.WriteLine($"Broj {broj} je deljiv sa 5");
+                }
+                else
+                {
+                    Console.WriteLine($"Broj {broj} nije deljiv sa 5");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
